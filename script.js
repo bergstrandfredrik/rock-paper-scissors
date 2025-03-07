b49eab24406a26a90dbf186cb71dd5b70ed688b1
Fredrik Bergstrand <fred.bergstrand@gmail.com>
2025-03-07 20:10:28 +0100

    Add function getComputerChoice

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-console.log("hello");
-
 /* 
 psuodo code for this project.
 
-Get the computers choice
+Get the computers choice ---- DONE
 Then the players choice
 now play the game and lets see who beat who.
 display the result and keep the score as long as the game is played.
 */
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+  const choice = ["rock", "paper", "scissors"];
+  return choice[getRandomNumber(3)];
+}
+
+console.log(getComputerChoice());
