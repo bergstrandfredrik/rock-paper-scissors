ff0a30627d5d14f610bbf62242075dc3303b91e0
Fredrik Bergstrand <fred.bergstrand@gmail.com>
2025-03-07 19:56:16 +0100

    Add psuodo code for project

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hello");
+
+/* 
+psuodo code for this project.
+
+Get the computers choice
+Then the players choice
+now play the game and lets see who beat who.
+display the result and keep the score as long as the game is played.
+*/
